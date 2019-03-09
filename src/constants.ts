d6699667dd890708d92a7420dada906dede388ff
mvrlin <mvrlin@pm.me>
2019-03-09 09:15:17 +0300

    feat: add parsers loader

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+// Default user-agent.
+export const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
--- src/sneakerbot.ts
+++ src/sneakerbot.ts
@@ -1,11 +1,35 @@
 import consola from 'consola'
 import puppeteer from 'puppeteer'
 
+import { readdirSync } from 'fs'
+import { USER_AGENT } from './constants'
+
 export default class SneakerBot {
   browser: puppeteer.Browser | null
 
   constructor () {
     this.browser = null
+  }
+
+  private async loadParsers () {
+    const dir = `${__dirname}/parsers`
+    const files = readdirSync(dir)
+
+    if (!this.browser) {
+      throw new Error('Browser cannot be null.')
+    }
+
+    if (!files.length) {
+      throw new Error('No parsers found.')
+    }
+
+    for (const file of files) {
+      const page = await this.browser.newPage()
+      await page.setUserAgent(USER_AGENT)
+
+      const parser = (await import(`${dir}/${file}`)).default
+      parser(page)
+    }
   }
 
   async start () {
@@ -22,6 +46,12 @@ export default class SneakerBot {
         '--window-size=1280x720'
       ]
     })
+
+    try {
+      await this.loadParsers()
+    } catch (error) {
+      return consola.error(error)
+    }
 
     consola.info('SneakerBot has been started.')
   }
